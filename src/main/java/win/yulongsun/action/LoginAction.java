bb9021631168a1b7e9843f488c05bba36cb2d82a
yulongsun007 <yulongsun0425@gmail.com>
2016-10-11 17:34:27 +0800

    1. 初始化

---
+++ src/main/java/win/yulongsun/action/LoginAction.java
@@ -0,0 +1,16 @@
+package win.yulongsun.action;
+
+import win.yulongsun.base.BaseAction;
+
+/**
+ * Created by : yulongsun
+ * Date on : 2016/10/11
+ * Desc :
+ */
+public class LoginAction extends BaseAction {
+
+    @Override
+    public String execute() throws Exception {
+        return super.execute();
+    }
+}
---
+++ src/main/java/win/yulongsun/base/BaseAction.java
@@ -0,0 +1,11 @@
+package win.yulongsun.base;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by : yulongsun
+ * Date on : 2016/10/11
+ * Desc :
+ */
+public class BaseAction extends ActionSupport {
+}
